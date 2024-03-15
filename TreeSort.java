d599a2a2cd521856ffd6bba3a4f0fbef4c73524f
NataliaVrabcova <161033336+NataliaVrabcova@users.noreply.github.com>
2024-03-15 12:29:07 +0000

    created TreeSort class

---
+++ TreeSort.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+import java.util.ArrayList;
+
+public class TreeSort {
+
+  public static void buildTree(Int[] array) {
+    for (int i = 0; i < array.length; i++) {
+      sortlist(list, i);
+    }
+  }
+  public static void sortlist(Int[] array) {
+
+
+  }
+}
