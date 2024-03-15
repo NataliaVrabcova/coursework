f2cf7df6ae88a00fd78cc9dc9d4b249af52b477d
NataliaVrabcova <161033336+NataliaVrabcova@users.noreply.github.com>
2024-03-15 12:27:27 +0000

    added arraylist

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,13 @@
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    ArrayList<Integer> list = new ArrayList<Integer>();
+    list.add(4);
+    list.add(2);
+    list.add(3);
+    list.add(5);
+    list.add(1);
+    for (int i : list) {
+      System.out.println(i);
+    }
   }
-
- 
 }
