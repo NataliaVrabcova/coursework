358bb65d9fe9518697f074ffeea4f1daf951b927
NataliaVrabcova <161033336+NataliaVrabcova@users.noreply.github.com>
2024-03-15 13:27:30 +0000

    wrote a Node

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 public class Main {
   public static void main(String[] args) {
     ArrayList<Integer> list = new ArrayList<Integer>();
@@ -6,8 +7,6 @@ public class Main {
     list.add(3);
     list.add(5);
     list.add(1);
-    for (int i : list) {
-      System.out.println(i);
-    }
+    System.out.println(list);
   }
 }
---
+++ src/main/java/TreeSort.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+import java.util.ArrayList;
+
+public class TreeSort {
+
+  private static void Node {
+    int data;
+    Node child_left;
+    Node child_right;
+    Node(int data){
+      this.data = data;
+      this.child_left = null;
+      this.child_right = null;
+    }
+  }
+  private Node = root;
+  
+  privete static void add_item(int data) {
+    
+  }
+
+  public static void walk_tree(Int[] array) {
+
+  }
+
+  public static void buildTree(String[] list) {
+    for (int i = 0; i < list.length; i++) {
+      walk_tree(list, i);
+    }
+  }
+}
