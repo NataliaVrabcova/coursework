c12c5ecf775e71568ed62d1934f765485ff46b86
NataliaVrabcova <161033336+NataliaVrabcova@users.noreply.github.com>
2024-03-17 14:56:35 +0000

    added add_item

--- src/main/java/TreeSort.java
+++ src/main/java/TreeSort.java
@@ -1,9 +1,7 @@
 import java.util.Arrays;
 import java.util.ArrayList;
 
-public class TreeSort {
-
-  private static void Node {
+public class Node {
     int data;
     Node child_left;
     Node child_right;
@@ -12,20 +10,29 @@ public class TreeSort {
       this.child_left = null;
       this.child_right = null;
     }
-  }
-  private Node = root;
-  
-  privete static void add_item(int data) {
+   void add_item(int item){
+     if (item < data) {
+         if (child_left == null) {
+             child_left = new Node(item);
+         } else {
+             child_left.add(item);
+         }
+     } else {
+         if (child_right == null) {
+             child_right = new Node(item);
+         } else {
+             child_right.add(item);
+         }
+     }
+     
+   }
     
   }
+}
 
-  public static void walk_tree(Int[] array) {
-
-  }
 
   public static void buildTree(String[] list) {
     for (int i = 0; i < list.length; i++) {
       walk_tree(list, i);
     }
   }
-}
