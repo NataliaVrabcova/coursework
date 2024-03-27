12961e4b147e876d6035c883608c97ef0426d4ba
NataliaVrabcova <161033336+NataliaVrabcova@users.noreply.github.com>
2024-03-27 13:23:08 +0000

    created Test code to check

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,15 +1,19 @@
 import java.util.ArrayList;
 import java.util.List;
-
 public class Main {
-    public static void main(String[] args) {
-        ArrayList<Integer> list = new ArrayList<>();
-        list.add(4);
-        list.add(2);
-        list.add(3);
-        list.add(5);
-        list.add(1);
-        List<Integer> sorted = TreeSort.treeSort(list);
-        System.out.println("Sorted list: " + sorted);
+  public static void main(String[] args) {
+    int[] inputArray = {4, 2, 3, 5, 1}; // Input array
+    List<Integer> inputList = new ArrayList<>();
+    for (int num : inputArray) {
+        inputList.add(num);
     }
+    List<Integer> treesorted = TreeSort.treeSort(inputList);
+
+    int[] sortedArray = {1, 2, 3, 4, 5}; // sorted array
+    List<Integer> sorted = new ArrayList<>();
+    for (int num : sortedArray) {
+        sorted.add(num);
+    }
+   
+  }
 }
--- src/main/java/TreeSort.java
+++ src/main/java/TreeSort.java
@@ -1,10 +1,10 @@
 import java.util.List;
 
 public class TreeSort {
-    public static List<Integer> treeSort(List<Integer> list) {
-        Node root = new Node(list.get(0));
-        for (int i = 1; i < list.size(); i++) {
-            root.add(list.get(i));
+    public static List<Integer> treeSort(List<Integer> input) {
+        Node root = new Node(input.get(0));
+        for (int i = 1; i < input.size(); i++) {
+            root.add(input.get(i));
         }
         return root.walk();
     }
