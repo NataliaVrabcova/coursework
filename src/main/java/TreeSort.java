9902f11a3e52d5700e3b196ec6cb6efb94d05d32
NataliaVrabcova <161033336+NataliaVrabcova@users.noreply.github.com>
2024-03-27 14:09:23 +0000

    fixed test and added it to files

--- src/main/java/TreeSort.java
+++ src/main/java/TreeSort.java
@@ -1,10 +1,10 @@
 import java.util.List;
 
 public class TreeSort {
-    public static List<Integer> treeSort(List<Integer> input) {
-        Node root = new Node(input.get(0));
-        for (int i = 1; i < input.size(); i++) {
-            root.add(input.get(i));
+    public static List<Integer> treeSort(List<Integer> inputList) {
+        Node root = new Node(inputList.get(0));
+        for (int i = 1; i < inputList.size(); i++) {
+            root.add(inputList.get(i));
         }
         return root.walk();
     }
